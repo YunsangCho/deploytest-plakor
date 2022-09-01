7689a282f6bee415f79259ca4ef8ecd2c24e7c74
YunsangCho <dddbstkd@gmail.com>
2022-09-01 13:36:34 +0900

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      deployTest
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app  = express();
+
+app.use(express.static('build'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+});
+
+app.listen(4000, () => {
+    console.log("Backend is running!");
+});
+
+//client build → build 폴더 → server/build → 서버 실행
+
+
+//-----------------------
+// client 폴더로 이동 
+// npm ci 로 node_module 생성
+// npm run build
+// client/build → server/build 폴더로 이동
+// 서버에서 npm ci 해서 node_module 생성
+// tsc  타입스크립트 → 자바스크립트로 컴파일
+// node app.js
+//--------------------------
+
+//↑ 이 과정을 스크립트로 짜기
